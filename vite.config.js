72a34449bfb0f8fbb4c22a09980136d56669f1aa
Mariam_Tarek <mariamelsarag44@gmail.com>
2023-09-08 03:20:21 +0300

    upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "https://github.com/Mariamtarek514/tip-calculator",
     plugins: [react()],
 });
